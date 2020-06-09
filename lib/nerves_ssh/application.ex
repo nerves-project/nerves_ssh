9d371802c7713ee46bb0a51d2ca04d4eae482bef
Frank Hunleth <fhunleth@troodon-software.com>
2020-06-09 16:33:50 -0400

    Fix option resolution

--- lib/nerves_ssh/application.ex
+++ lib/nerves_ssh/application.ex
@@ -12,8 +12,6 @@ defmodule NervesSSH.Application do
   def start(_type, _args) do
     opts =
       Application.get_all_env(:nerves_ssh)
-      |> resolve_firmware_ssh_authorized_keys()
-      |> resolve_firmware_ssh_system_dir()
       |> resolve_system_dir()
       |> Options.new()
       |> Options.sanitize()
@@ -24,47 +22,16 @@ defmodule NervesSSH.Application do
     Supervisor.start_link(children, opts)
   end
 
-  defp resolve_firmware_ssh_authorized_keys(opts) do
-    # Check for legacy nerves_firmware_ssh :authorized_keys so that if they still
-    # exist, they get merged with a warning.
-    case Application.get_env(:nerves_firmware_ssh, :authorized_keys) do
-      keys when is_list(keys) ->
-        Logger.warn(
-          "ssh authorized keys found in :nerves_firmware_ssh config. Please move them to :nerves_ssh in your config.exs."
-        )
-
-        # Merge with nerves_ssh keys or set if none were specified
-        Keyword.update(opts, :authorized_keys, keys, &Enum.dedup(&1 ++ keys))
-
-      _other ->
-        opts
-    end
-  end
-
-  defp resolve_firmware_ssh_system_dir(opts) do
-    # Check for legacy nerves_firmware_ssh :system_dir so that if it still exists
-    # but is not in :nerves_ssh, it is merged with a warning
-    system_dir = Application.get_env(:nerves_firmware_ssh, :system_dir)
-
-    if system_dir and is_nil(opts[:system_dir]) do
-      Logger.warn(
-        "ssh system directory found in :nerves_firmware_ssh config. Please move it to :nerves_ssh in your config.exs"
-      )
-    end
-
-    Keyword.put_new(opts, :system_dir, system_dir)
-  end
-
   defp resolve_system_dir(opts) do
     cond do
-      system_dir = opts[:system_dir] ->
-        system_dir
+      opts[:system_dir] ->
+        opts
 
       File.dir?(@default_system_dir) and host_keys_readable?(@default_system_dir) ->
-        @default_system_dir
+        Keyword.put(opts, :system_dir, @default_system_dir)
 
       true ->
-        :code.priv_dir(:nerves_ssh)
+        Keyword.put(opts, :system_dir, :code.priv_dir(:nerves_ssh))
     end
   end
 
