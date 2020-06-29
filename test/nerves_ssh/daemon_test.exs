a6d59a7504c529f825fc8b2c0c0e4b79b831f3dd
Frank Hunleth <fhunleth@troodon-software.com>
2020-06-28 22:26:25 -0400

    Handle daemon surviving past terminate/2
    Dialyzer passes without the "ignore" list now.

--- test/nerves_ssh/daemon_test.exs
+++ test/nerves_ssh/daemon_test.exs
@@ -41,4 +41,40 @@ defmodule NervesSSH.DaemonTest do
 
     assert {:ok, "4", 0} == ssh_run("2 + 2")
   end
+
+  test "stopping and starting the application" do
+    assert {:ok, ":running_at_start?", 0} == ssh_run(":running_at_start?")
+
+    assert :ok == Application.stop(:nerves_ssh)
+    Process.sleep(500)
+    assert {:error, :econnrefused} == ssh_run(":really_stopped?")
+
+    assert :ok == Application.start(:nerves_ssh)
+    assert {:ok, ":started_again?", 0} == ssh_run(":started_again?")
+  end
+
+  test "starting the application after terminate wasn't called" do
+    assert :ok == Application.stop(:nerves_ssh)
+
+    # Start a server up manually to simulate terminate not being called
+    # to shut down the server.
+    {:ok, _pid} =
+      GenServer.start(
+        NervesSSH.Daemon,
+        NervesSSH.Options.new(
+          user_passwords: [{"test_user", "not_the_right_password"}],
+          port: 4022,
+          system_dir: :code.priv_dir(:nerves_ssh)
+        )
+      )
+
+    # Verify that the old server has started and that it won't accept
+    # the test credentials.
+    assert {:error, 'Unable to connect using the available authentication methods'} ==
+             ssh_run(":started_again?")
+
+    # Start the real server up. It should kill our old one.
+    assert :ok == Application.start(:nerves_ssh)
+    assert {:ok, ":started_again?", 0} == ssh_run(":started_again?")
+  end
 end
