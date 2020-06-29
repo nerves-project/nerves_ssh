e51b51a4616c4bac10b8f25dab74962c539f265f
Frank Hunleth <fhunleth@troodon-software.com>
2020-06-29 09:38:12 -0400

    Add NervesSSH.info/0
    This returns the result of `:ssh.daemon_info/1` and is useful for
    debugging server issues.

--- lib/nerves_ssh.ex
+++ lib/nerves_ssh.ex
@@ -8,4 +8,12 @@ defmodule NervesSSH do
   """
   @spec configuration :: NervesSSH.Options.t()
   defdelegate configuration(), to: NervesSSH.Daemon
+
+  @doc """
+  Return information on the running ssh daemon.
+
+  See [ssh.daemon_info/1](http://erlang.org/doc/man/ssh.html#daemon_info-1).
+  """
+  @spec info() :: {:ok, [:ssh.daemon_info_tuple()]} | {:error, :bad_daemon_ref}
+  defdelegate info(), to: NervesSSH.Daemon
 end
