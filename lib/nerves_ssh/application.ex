c51b17de0dcbbf55d13ff0dcad0f71fcca560f26
Frank Hunleth <fhunleth@troodon-software.com>
2020-08-18 09:13:59 -0400

    Detect out-of-date config.exs at compile time
    When upgrading from `:nerves_firmware_ssh` to `:nerves_ssh`, it's easy
    to forget to modify the `config.exs`. This adds a big compile-time error
    to tell you what to do.

--- lib/nerves_ssh/application.ex
+++ lib/nerves_ssh/application.ex
@@ -5,6 +5,21 @@ defmodule NervesSSH.Application do
 
   alias NervesSSH.Options
 
+  if Application.get_all_env(:nerves_ssh) == [] and
+       Application.get_all_env(:nerves_firmware_ssh) != [] do
+    raise """
+    :nerves_ssh isn't configured, but :nerves_firmware_ssh is.
+
+    This is probably not right. If you recently upgraded to :nerves_ssh or
+    a library that uses it like :nerves_pack, you'll need to edit your config.exs
+    and rename references to :nerves_firmware_ssh to :nerves_ssh. See
+    https://hexdocs.pm/nerves_ssh/readme.html#configuration.
+
+    To use both :nerves_ssh and :nerves_firmware_ssh simultaneously, supply a
+    :nerves_ssh config to bypass this error.
+    """
+  end
+
   @impl Application
   def start(_type, _args) do
     children =
