d559d7ad42ebab8baa3c8d0cd3692b0d2132d7dd
Frank Hunleth <fhunleth@troodon-software.com>
2021-11-23 21:50:29 -0500

    Fix Dialyzer errors

--- lib/nerves_ssh/exec.ex
+++ lib/nerves_ssh/exec.ex
@@ -30,7 +30,7 @@ defmodule NervesSSH.Exec do
   @doc """
   Run one LFE command coming over ssh
   """
-  @spec run_lfe(charlist()) :: {:ok, binary()} | {:error, binary()}
+  @spec run_lfe(charlist()) :: {:ok, iolist()} | {:error, binary()}
   def run_lfe(cmd) do
     {value, _} = :lfe_shell.run_string(cmd)
     {:ok, :lfe_io.prettyprint1(value, 30)}
