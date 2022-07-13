6c46a75be3f786c79e196a101c19f96e044cb633
Steffen Deusch <steffen@deusch.me>
2022-07-13 19:57:09 +0200

    allow erlexec to run as root

--- config/config.exs
+++ config/config.exs
@@ -8,3 +8,8 @@ config :nerves_runtime,
   target: "host"
 
 config :nerves_runtime, Nerves.Runtime.KV.Mock, %{"nerves_fw_devpath" => "/dev/will_not_work"}
+
+config :erlexec,
+  root: true,
+  user: "root",
+  limit_users: ["root"]
