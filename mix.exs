a505298f81017e6f98daf40a7a3949993a5ddffc
Jon Carstens <jjcarstens@me.com>
2023-06-03 11:08:28 -0600

    Drop support for Elixir 1.10
    Nerves requires Elixir >= 1.11.2, so this brings NervesSSh in sync
    with that and also aids in supporting Elixir 1.15

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule NervesSSH.MixProject do
     [
       app: :nerves_ssh,
       version: @version,
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
