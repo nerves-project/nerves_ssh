04a997e606f37353e3bdcfe5815a9c9b62dd1da8
Frank Hunleth <fhunleth@troodon-software.com>
2024-06-24 08:19:57 -0400

    Require Elixir 1.13
    This was instigated by credo not working on Elixir 1.11 any more. Other
    libraries are requiring Elixir 1.13 and later now anyway, so this
    probably doesn't affect anyone in practice.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule NervesSSH.MixProject do
     [
       app: :nerves_ssh,
       version: @version,
-      elixir: "~> 1.11",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
