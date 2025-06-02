9305531276a52d60ef1c855923123e5e6188b6f0
Ben Youngblood <bx.youngblood@gmail.com>
2025-06-02 14:04:50 -0700

    Drop support for Elixir 1.13

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule NervesSSH.MixProject do
     [
       app: :nerves_ssh,
       version: @version,
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
